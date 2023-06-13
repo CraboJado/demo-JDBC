a180fc02dcf7c949c435811462a12273bd33c9cb
liting <liting_qiu@hotmail.com>
2023-06-13 09:52:12 +0200

    TP3 EX2 fait

---
+++ src/main/java/fr/diginamic/jdbc/TestUpdate.java
@@ -0,0 +1,32 @@
+package fr.diginamic.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class TestUpdate {
+    private static final String DB_URL;
+    private static final String DB_USER;
+    private static final String DB_PSW;
+
+    static {
+        ResourceBundle monFichierConf = ResourceBundle.getBundle("db");
+        DB_URL = monFichierConf.getString("db.DB_URL");
+        DB_USER = monFichierConf.getString("db.DB_USER");
+        DB_PSW = monFichierConf.getString("db.DB_PSW");
+    }
+
+    public static void main(String[] args) {
+        try (Connection maConnection = DriverManager.getConnection(DB_URL, DB_USER, DB_PSW);
+             Statement monStatement = maConnection.createStatement();
+        ) {
+            int nb = monStatement.executeUpdate("UPDATE FOURNISSEUR SET NOM = 'La Maison des Peintures' WHERE ID = 4");
+            System.out.println(nb);
+        } catch (SQLException e) {
+            System.err.println("err is " + e);
+//            throw new RuntimeException(e);
+        }
+    }
+}
