d3d5ad212f70f7d40b1b283402e78c0478881b6c
liting <liting_qiu@hotmail.com>
2023-06-14 22:24:53 +0200

    TP4 corrigé : implementer SLF4J

--- src/main/java/fr/diginamic/jdbc/TestFournisseurDAOjdbc.java
+++ src/main/java/fr/diginamic/jdbc/TestFournisseurDAOjdbc.java
@@ -2,12 +2,17 @@ package fr.diginamic.jdbc;
 
 import fr.diginamic.jdbc.dao.FournisseurDaoJdbc;
 import fr.diginamic.jdbc.entites.Fournisseur;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class TestFournisseurDAOjdbc {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestFournisseurDAOjdbc.class);
     public static void main(String[] args) {
+
         try{
             FournisseurDaoJdbc fourDao = new FournisseurDaoJdbc();
             // lister les fournisseurs
@@ -28,7 +33,7 @@ public class TestFournisseurDAOjdbc {
             fourDao.delete(newFour);
 
         }catch (Exception e){
-            System.out.println(e);
+            LOG.error(e.getMessage());
         }
 
     }
